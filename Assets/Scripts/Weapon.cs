7df480af0f8f2c39704732271b3d56e31a2f28f9
Sont3k <markus.residence@gmail.com>
2020-06-13 15:00:08 +0300

    176. Introduction To Raycasting

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Camera FPSCamera;
+    public float range = 100f;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        Physics.Raycast(FPSCamera.transform.position, FPSCamera.transform.forward, out hit, range);
+
+        if (hit.transform != null)
+        {
+            print(hit.transform.gameObject.name);
+        }
+    }
+}

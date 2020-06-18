33870466d0203bc76a66ad4abedada1fe993b333
Sont3k <markus.residence@gmail.com>
2020-06-18 11:55:56 +0300

    185. Create Player Health Class

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    private PlayerHealth target;
+    public float damage = 40f;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        target = FindObjectOfType<PlayerHealth>();
+    }
+
+    public void AttackHitEvent()
+    {
+        if (target)
+        {
+            target.TakeDamage(damage);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float health = 10f;
+
+    public void TakeDamage(float damage)
+    {
+        if (health > damage)
+        {
+            health -= damage;
+        }
+        else
+        {
+            Destroy(gameObject);
+            print("Player is dead");
+        }
+    }
+}

0ced477109251e7d07bcf934052af860aa0a09c8
Sont3k <markus.residence@gmail.com>
2020-06-23 21:17:37 +0300

    192. Weapon Zoom - Field Of View

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour
+{
+    public Camera mainCamera;
+    public float zoomedOutFOV = 60f;
+    public float zoomedInFOV = 20f;
+    private bool zoomedInToggle;
+
+    private void Update()
+    {
+        ToggleZoom();
+    }
+
+    private void ToggleZoom()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (!zoomedInToggle)
+            {
+                zoomedInToggle = true;
+                mainCamera.fieldOfView = zoomedInFOV;
+            }
+            else
+            {
+                zoomedInToggle = false;
+                mainCamera.fieldOfView = zoomedOutFOV;
+            }
+        }
+    }
+}
